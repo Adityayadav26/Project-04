0e0c054b3a6e4e475ef4290dd075b829db0d85df
Adityayadav26 <adityayadav7806@gmail.com>
2026-02-10 15:34:39 +0530

    done"

---
+++ ORSProject-04/src/main/java/in/co/rays/proj4/App.java
@@ -0,0 +1,13 @@
+package in.co.rays.proj4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
